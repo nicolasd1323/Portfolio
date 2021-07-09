deeadd0257075d277fa785b96fea0dd1ea6c09a3
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-07-09 15:25:07 -0400

    resume

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -13,8 +13,11 @@ function Home(props) {
           <h1 className="nico">Hola, yo soy Nicolas,</h1>
           <h1 className="nico">Salut, je suis Nicolas.</h1>
         </div>
-        <div>
+        <div className="resume">
           <h2>Junior Software Engineer</h2>
+          <h5>RESUME</h5>
+      <a href="https://drive.google.com/file/d/1ko9SIeW_QT48jBGpXCwg64SxA1FK_iTT/view"><img className="image-PORTFOLIO"src="https://i.ibb.co/BB4Zzm3/Screen-Shot-2021-07-09-at-3-08-39-PM.png"></img></a>
+
         </div>
         <div className="about-me">
           <h4>About Me</h4>
@@ -30,7 +33,7 @@ function Home(props) {
           </p>
         </div>
         <div className="link-to-portfolio">
-          <h3>WANT TO SEE MY WORK!? TAKE A LOOK TO ALL MY PROJECTS</h3>
+          <h3>WANT TO SEE MY WORK!? TAKE A LOOK AT MY PROJECTS</h3>
           <Link className="portfolio-link-b" to="/portfolio-products">
             <p>PORTFOLIO</p>
           </Link>
